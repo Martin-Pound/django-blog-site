5b8889b45ea138f2627f645010023790f0e5a6b1
Martin-Pound <mpound001@gmail.com>
2025-08-30 09:39:47 +0100

    committing latest updates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,71 @@
+from datetime import date
 from django.shortcuts import render
 
-# Create your views here.
+#CONSTANTS
+all_posts = [
+    {
+        "slug": "Surfing-Addiction",
+        "image": "Surfing.jpeg",
+        "author": "Martin Pound",
+        "date": date(2025, 3, 15),
+        "title": "Surfing Addiction",
+        "excerpt": "Why I wish I never knew the feeling of dropping into a wave.",
+        "content": """
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            
+            lorwm ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+        """
+    },
+    {
+        "slug": "city-food-tour",
+        "image": "city-food.jpg",
+        "author": "Jane Smith",
+        "date": date(2025, 5, 15),
+        "title": "Discovering Culinary Delights",
+        "excerpt": "A gastronomic adventure through the city's best eateries and hidden gems.",
+        "content": """
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            """
+    },
+    {
+        "slug": "art-gallery-opening",
+        "image": "art-gallery.png",
+        "author": "Emily Johnson",
+        "date": date(2025, 7, 15),
+        "title": "Exploring Contemporary Art",
+        "excerpt": "An immersive experience into modern art and creativity.",
+        "content": """
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            """
+    },
+]
 
+#FUNCTIONS
+
+def get_date(post):
+    return post['date'] #Function to get the date of a post
+
+#VIEWS
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date) #Sort posts by date
+    latest_posts = sorted_posts[-3:] #Get the latest 3 posts
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html", {
+          "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    pass
+    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
