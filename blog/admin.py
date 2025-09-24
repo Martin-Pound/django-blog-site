c065dd31849c26f168f6c286f4529ab1db7c8cfd
Martin-Pound <mpound001@gmail.com>
2025-09-24 10:56:58 +0100

    Latest models and admin updates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
+from symtable import Class
+
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)} #automatically populates the slug field based on the title field
+    list_filter = ("author", "date", "tags") #adds filters in the admin interface for these fields
+    list_display = ("title", "author", "date") #displays these fields in the admin list view
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email_address") #displays the full name of the author in the admin list view
+    search_fields = ("first_name", "last_name") #adds a search bar to search authors by first or last name
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("caption",) #displays the tag caption in the admin list view
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email_address = models.EmailField() # EmailField validates email format
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -15,7 +15,7 @@ class Author(models.Model):
         verbose_name_plural = "Authors"
 
 class Tag(models.Model):
-    caption = models.CharField(max_length=50)
+    caption = models.CharField(max_length=20)
 
     def __str__(self):
         return self.caption
@@ -26,13 +26,12 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="blog", null=True, blank=True)
-    date = models.DateField()
-    slug = models.SlugField(unique=True)
+    image = models.CharField(max_length=100) # TEMPORARY: URL or path to the image
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True) # Unique slug for URL references title
     content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts") # Foreign key relationship with Author
-    tags = models.ManyToManyField(Tag, related_name="posts") # Many-to-many relationship with Tag
-
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts") # Foreign key relationship with Author
+    tags = models.ManyToManyField(Tag) # Many-to-many relationship with Tag
 
     def get_absolute_url(self):
         return reverse("post_detail", args=[self.slug]) # URL to access a particular post instance
