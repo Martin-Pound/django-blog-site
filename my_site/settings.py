4a1fe462fc3aae08cb120354256d1b3230f13c77
Martin-Pound <mpound001@gmail.com>
2025-10-22 11:59:29 +0100

    Adding enviroment keys

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tyhfu4wi&!bn8n9!$l)1qbd#ey+h_5tah)6@1aew)d25=7ni3n'
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
